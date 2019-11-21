b6963d8c50dbd873b79eff7c89ddab80d539d4b8
Unhackable <35536717+mypasswordismyusername@users.noreply.github.com>
2019-11-21 10:07:04 -0600

    Team3 update 3
    changed the strat

--- team3.py
+++ team3.py
@@ -25,15 +25,14 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    if their_history == 'b':
+    if their_history == 'b' or their_history == 'B':
         return 'b'
-    elif their_history == 'c':
+    elif their_history == 'c' or their_history == 'C':
         return 'b'
     else:
         return 'c'
         
-    return 'c'
-
+   
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
